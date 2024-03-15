d94ec148d4f12a8dade5b3c9324618cdb374dfdf
Your Name <florian.eusterholz@gmail.com>
2024-03-15 17:11:19 +0100

    json added to script

---
+++ script.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+        "question" : "Wie viel frisst ein Elefant täglich?",
+        "answer_1" : "10 kg",
+        "answer_2" : "150 kg",
+        "answer_3" : "300 kg",
+        "answer_4" : "90 kg",
+        "right_answer" : "2"
+    },
+    {
+        "question" : "Wie schnell kann ein Bär rennen?",
+        "answer_1" : "20 km/h",
+        "answer_2" : "80 km/h",
+        "answer_3" : "35 km/h",
+        "answer_4" : "45 km/h",
+        "right_answer" : "4"
+    },
+    {
+        "question" : "Aus wie viel Steinen besteht die Cheops-Pyramide in Gizeh?",
+        "answer_1" : "10 Millionen",
+        "answer_2" : "2 Millionen",
+        "answer_3" : "40 Millionen",
+        "answer_4" : "500.000",
+        "right_answer" : "2"
+    },
+    {
+        "question" : "Wann fanden die ersten Olympischen Spiele statt?",
+        "answer_1" : "1824 n. Chr.",
+        "answer_2" : "420 n. Chr",
+        "answer_3" : "776 v. Chr.",
+        "answer_4" : "123 v. Chr.",
+        "right_answer" : "3"
+    },
+    {
+        "question" : "Wie viele Planeten besitzt unser Sonnensystem?",
+        "answer_1" : "9",
+        "answer_2" : "8",
+        "answer_3" : "7",
+        "answer_4" : "6",
+        "right_answer" : "3"
+    },
+    {
+        "question" : "Wer führte die Germanen in die Varusschlacht 9 n. Chr.?",
+        "answer_1" : "Maximus",
+        "answer_2" : "Thor",
+        "answer_3" : "Arminius",
+        "answer_4" : "Hans",
+        "right_answer" : "3"
+    }
+];
